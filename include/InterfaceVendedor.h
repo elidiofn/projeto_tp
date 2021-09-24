e13187643e6f27bbb716a6c2100e814b6e9c996c
Elidio F Nascimento <89420290+elidiofn@users.noreply.github.com>
2021-09-23 21:07:45 -0300

    beta 1.0

--- include/InterfaceVendedor.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERFACEVENDEDOR_H_INCLUDED
-#define INTERFACEVENDEDOR_H_INCLUDED
-
-void realizar_venda();
-void relatorio_minhas_vendas();
-void consultar_estoque();
-void menu_vendedor();
-
-#endif // INTERFACEVENDEDOR_H_INCLUDED
